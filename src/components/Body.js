020b207a3497741de934ad418d929eaa3cda3f8f
Joe Stephen <joestephenk10@gmail.com>
2024-08-19 15:38:24 +0530

    UI updations.

--- src/components/Body.js
+++ src/components/Body.js
@@ -9,7 +9,7 @@ const Body = () => {
     { path: "/browse", element: <Browse /> },
   ]);
   return (
-    <div>
+    <div >
       <RouterProvider router={appRouter} />
     </div>
   );
--- src/components/VIdeoTitle.js
+++ src/components/VIdeoTitle.js
@@ -6,14 +6,14 @@ const VIdeoTitle = ({ title, overview }) => {
   return (
     <div>
       <div className="w-screen aspect-video pt-[20%] px-12 absolute text-white bg-gradient-to-r from-black">
-        <h1 className="text-6xl font-bold">{title}</h1>
-        <p className="py-6 text-lg w-1/3">{overview}</p>
+        <h1 className="text-5xl font-bold">{title}</h1>
+        <p className="py-6 text-sm w-1/3">{overview}</p>
         <div className="flex">
-          <button className="flex text-center bg-white text-black p-4 px-12 text-lg  rounded-md hover:bg-opacity-80">
+          <button className="flex text-center bg-white text-black p-4 px-12 text-lg items-center rounded-md hover:bg-opacity-80">
             <FaPlay />
             Play
           </button>
-          <button className="flex bg-gray-400 text-white mx-2 p-4 px-12 text-lg bg-opacity-50 rounded-md hover:bg-opacity-80">
+          <button className="flex bg-gray-400 text-white mx-2 p-4 px-12 text-lg items-center bg-opacity-50 rounded-md hover:bg-opacity-80">
             <SlInfo /> More Info
           </button>
         </div>
